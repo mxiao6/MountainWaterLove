4b2ea857276c043044c059fd52fd74ce7814e5fd
Mingze Xiao <mxiao666@gmail.com>
2017-05-07 01:27:38 -0500

    music script

---
+++ mp3/playMusic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playMusic : MonoBehaviour {
+
+	public float delayTime;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+		audioSource.PlayDelayed( delayTime );
+	}
+	
+	// Update is called once per frame
+	// void Update () {
+	
+	// }
+}
