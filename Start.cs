11eb4b9fc5ae04aac536eb36f410f7ef42de6be5
Mingze Xiao <mxiao666@gmail.com>
2017-05-09 17:43:07 -0500

    add chenge scene script

---
+++ Start.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetButtonDown("Y")) {
+			Application.LoadLevel("Scene Terrain");
+		}
+	}
+}
