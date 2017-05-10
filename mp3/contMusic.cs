a895e0594ca448294491b505b7976ad25c914c3f
Mingze Xiao <mxiao666@gmail.com>
2017-05-09 21:25:10 -0500

    add background music and script:

---
+++ mp3/contMusic.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playMusic : MonoBehaviour {
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+		DontDestroyOnLoad(audioSource);
+	}
+	
+	// Update is called once per frame
+	// void Update () {
+	
+	// }
+}
